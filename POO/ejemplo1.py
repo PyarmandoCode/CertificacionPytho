3bd1481291743d4b051254230b74e68d30913c8f
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2025-05-05 20:22:22 -0500

    Curso Python

---
+++ POO/ejemplo1.py
@@ -0,0 +1,14 @@
+#Clase es una plantilla principal
+#Poo
+class Persona:
+    pass
+
+
+# Objetos instanciarse de la clase
+juan  = Persona()
+pedro = Persona()
+miguel = Persona()
+
+print(juan)
+print(pedro)
+print(miguel)
